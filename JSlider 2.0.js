4e45048f2733d2816423b267d1e2b22dfb67342e
Daniel <danyjunor96@gmail.com>
2018-07-17 11:26:39 +0300

    Click events for buttons

--- JSlider 2.0.js
+++ JSlider 2.0.js
@@ -1,76 +1,95 @@
-// jslider = (parentElement, userSettings) => {
-
-  // const _defaultSettings = {
-  //   showElements:1,
-  //   scrollElements:1,
-  //   rowsCount:1,
-  //   animSpeed:500,
-  //   infinite:false,
-  //   centered:false,
-  //   startSlide:0,
-  //   currentSlideClass:'jslider-current',
-  //   previousButton:'.jslider-prev',
-  //   nextButton:'.jslider-next',
-  //   scrollDots:false,
-  //   lazyLoad:false,
-  //   autoPlay:false,
-  //   autoPlaySpeed:1000,
-  //   variableWidth:false,
-  //   highlightCurrent:true,
-  //   touch:true,
-  //   turn:'on'
-  // };
-  //
-  // let _settings = _defaultSettings;
-  //
-  // if(userSettings)
-  //   userSettings.map((e,i)=>{
-  //     _settings[e] = userSettings[e];
-  //   });
-// }
-
-class JSlider{
-  constructor(parentElement, userSettings){
+class JSlider {
+  constructor(parentElement, userSettings) {
     this._defaultSettings = {
-      showElements:1,
-      scrollElements:1,
-      rowsCount:1,
-      animSpeed:500,
-      infinite:false,
-      centered:false,
-      startSlide:0,
-      currentSlideClass:'jslider-current',
-      previousButton:'.jslider-prev',
-      nextButton:'.jslider-next',
-      scrollDots:false,
-      lazyLoad:false,
-      autoPlay:false,
-      autoPlaySpeed:1000,
-      variableWidth:false,
-      highlightCurrent:true,
-      touch:true,
-      turn:'on'
+      showElements: 1,
+      scrollElements: 1,
+      rowsCount: 1,
+      infinite: false,
+      centered: false,
+      startSlide: 0,
+      currentSlideClass: 'jslider-current',
+      previousButton: 'jslider-prev',
+      nextButton: 'jslider-next',
+      scrollDots: false,
+      lazyLoad: false,
+      autoPlay: false,
+      autoPlaySpeed: 1000,
+      variableWidth: false,
+      highlightCurrent: true,
+      touch: true,
+      turn: 'on'
     };
 
     this._settings = this._defaultSettings;
-    this.mainElement = parentElement;
-    console.log(this);
+    this.mainElement = document.getElementById(parentElement);
 
     this.updateSettings(userSettings);
     this.prepareHTMLStructure();
+    this.currentSlide = this._settings.startSlide - 1;
+    this.scrollTo(this._settings.currentSlide);
+    this.initControls();
   }
-  updateSettings(userSettings){
-    if(userSettings)
-      Object.keys(userSettings).map((e,i)=>{
+  updateSettings(userSettings) {
+    if (userSettings)
+      Object.keys(userSettings).map((e, i) => {
         this._settings[e] = userSettings[e];
       });
   }
-  prepareHTMLStructure(){
-    let elements = document.getElementById(this.mainElement).innerHTML;
-    document.getElementById(this.mainElement).innerHTML = `
+  prepareHTMLStructure() {
+    this.mainElement.className += ' jslider';
+    for (let i = 0; i < this.mainElement.children.length; i++) {
+      this.mainElement.children[i].className += " jslider-slide";
+    }
+    let elements = this.mainElement.innerHTML;
+
+    this.mainElement.innerHTML = `
       <div class="jslider-track">${elements}</div>
     `;
-    console.log(document.getElementById(this.mainElement).children[0].children);
-      // document.getElementById(this.mainElement).innerHTML = elements;
+
+    this.trackElement = this.mainElement.children[0];
+    this.slidesCount = this.trackElement.children.length;
+
+    this.styleElements();
   }
+  styleElements() {
+    this.sliderWidth = this.mainElement.offsetWidth;
+    this.slideWidth = this.sliderWidth / this._settings.showElements;
+    for (let i = 0; i < this.trackElement.children.length; i++) {
+      this.trackElement.children[i].style.width = this.slideWidth;
+    }
+    this.trackElement.style.width = this.trackElement.children.length * this.slideWidth;
+    this.mainElement.style.height = this.trackElement.offsetHeight;
+  }
+  scrollTo(slideNumber) {
+    console.log(slideNumber);
+    this.trackElement.style.left = 0 - this.slideWidth * (slideNumber);
+  }
+  initControls() {
+    const self = this;
+    document.getElementById(self._settings.previousButton).addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (self._settings.infinite == true)
+        self.currentSlide = self.currentSlide - 1 < 0 ?
+          self.slidesCount - this._settings.showElements : self.currentSlide - 1;
+      else if (self._settings.infinite == false)
+        self.currentSlide = self.currentSlide - 1 < 0 ?
+          0 : self.currentSlide - 1;
+      self.scrollTo(self.currentSlide);
+    });
+    document.getElementById(self._settings.nextButton).addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+
+      if (self._settings.infinite == true)
+        self.currentSlide = self.currentSlide + 1 == self.slidesCount-1 ?
+          0 : self.currentSlide + 1;
+      else if (self._settings.infinite == false)
+        self.currentSlide = self.currentSlide + 1 >= self.slidesCount-this._settings.showElements ?
+          self.slidesCount-this._settings.showElements : self.currentSlide + 1;
+      self.scrollTo(self.currentSlide);
+    });
+  }
+  
 }
