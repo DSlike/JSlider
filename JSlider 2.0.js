0abdd89c6c15e19a3a7d1e00f2285c124249853f
Daniel <danyjunor96@gmail.com>
2018-07-18 13:13:08 +0300

    Added scrolling dots

--- JSlider 2.0.js
+++ JSlider 2.0.js
@@ -8,14 +8,14 @@ class JSlider {
       centered: false,
       startSlide: 0,
       currentSlideClass: 'jslider-current',
-      previousButton: 'jslider-prev',
-      nextButton: 'jslider-next',
+      previousButton: '#jslider-prev',
+      nextButton: '#jslider-next',
       scrollDots: false,
       lazyLoad: false,
       autoPlay: false,
       autoPlaySpeed: 1000,
       variableWidth: false,
-      highlightCurrent: true,
+      spotlightCurrent: true,
       touch: true,
       turn: 'on'
     };
@@ -24,14 +24,13 @@ class JSlider {
     this.touchEnd = 0;
 
     this._settings = this._defaultSettings;
-    this.mainElement = document.getElementById(parentElement);
+    this.mainElement = document.querySelector(parentElement);
+    this.parentElementName = parentElement;
 
     this.updateSettings(userSettings);
     this.prepareHTMLStructure();
     this.currentSlide = this._settings.startSlide - 1;
     this.scrollTo(this._settings.currentSlide);
-    this.initControls();
-    this._settings.touch == true && this.initTouchEvents();
   }
   updateSettings(userSettings) {
     if (userSettings)
@@ -52,8 +51,18 @@ class JSlider {
 
     this.trackElement = this.mainElement.children[0];
     this.slidesCount = this.trackElement.children.length;
-
     this.styleElements();
+    this._settings.scrollDots && this.addScrollDots();
+  }
+  addScrollDots() {
+    let dots = `<div class="jslider-dots">`;
+    const dotsCount = Math.floor(this.slidesCount/this._settings.showElements)*this._settings.showElements;
+    for(let i=0; i<dotsCount; i++){
+      dots += `<button class="jslider-dot" data-id="${i}"></button>`;
+    }
+    dots += '</div>';
+    this.mainElement.innerHTML+=dots;
+    this.trackElement = this.mainElement.children[0];
   }
   styleElements() {
     this.sliderWidth = this.mainElement.offsetWidth;
@@ -63,64 +72,89 @@ class JSlider {
     }
     this.trackElement.style.width = this.trackElement.children.length * this.slideWidth;
     this.mainElement.style.height = this.trackElement.offsetHeight;
+    this.initControls();
   }
   scrollTo(slideNumber) {
     this.trackElement.style.left = 0 - this.slideWidth * (slideNumber);
+    this.spotlightCurrentElements();
+  }
+  spotlightCurrentElements(){
+    const self = this;
+
+    /* SCROLLING DOTS */
+    let dots = document.querySelectorAll(`${this.parentElementName} .jslider-dot`);
+
+    [].forEach.call(dots, function(el, i) {
+        i!=self.currentSlide ?
+          el.classList.remove("active") :
+          el.classList.add('active');
+    });
+
+    if(this._settings.spotlightCurrent == true){
+      let slides = document.querySelectorAll(`${this.parentElementName} .jslider-slide`);
+
+      [].forEach.call(slides, function(el, i) {
+          i!=self.currentSlide ?
+            el.classList.remove(self._settings.currentSlideClass) :
+            el.classList.add(self._settings.currentSlideClass);
+      });
+    }
   }
   initControls() {
     const self = this;
-    document.getElementById(self._settings.previousButton).addEventListener("click", (e) => {
+    document.querySelector(self._settings.previousButton).addEventListener("click", (e) => {
       e.preventDefault();
       e.stopPropagation();
 
       if (self._settings.infinite == true)
         self.currentSlide = self.currentSlide - 1 < 0 ?
-          self.slidesCount - this._settings.showElements : self.currentSlide - 1;
+        self.slidesCount - this._settings.showElements : self.currentSlide - 1;
       else if (self._settings.infinite == false)
         self.currentSlide = self.currentSlide - 1 < 0 ?
-          0 : self.currentSlide - 1;
+        0 : self.currentSlide - 1;
       self.scrollTo(self.currentSlide);
     });
-    document.getElementById(self._settings.nextButton).addEventListener("click", (e) => {
+    document.querySelector(self._settings.nextButton).addEventListener("click", (e) => {
       e.preventDefault();
       e.stopPropagation();
 
       if (self._settings.infinite == true)
-        self.currentSlide = self.currentSlide + 1 == self.slidesCount-1 ?
-          0 : self.currentSlide + 1;
+        self.currentSlide = self.currentSlide + 1 == self.slidesCount - 1 ?
+        0 : self.currentSlide + 1;
       else if (self._settings.infinite == false)
-        self.currentSlide = self.currentSlide + 1 >= self.slidesCount-this._settings.showElements ?
-          self.slidesCount-this._settings.showElements : self.currentSlide + 1;
+        self.currentSlide = self.currentSlide + 1 >= self.slidesCount - this._settings.showElements ?
+        self.slidesCount - this._settings.showElements : self.currentSlide + 1;
       self.scrollTo(self.currentSlide);
     });
+
+    this._settings.touch == true && this.initTouchEvents();
   }
-  initTouchEvents(){
-    for(let i=0; i<this.slidesCount; i++){
-      document.getElementsByClassName('jslider-slide')[i].addEventListener("touchstart", (e) => {
+  initTouchEvents() {
+    for (let i = 0; i < this.slidesCount; i++) {
+      document.querySelectorAll('.jslider-slide')[i].addEventListener("touchstart", (e) => {
         this.touchStart = e.touches[0].clientX;
         this.touch = true;
       });
-      document.getElementsByClassName('jslider-slide')[i].addEventListener("touchend", (e) => {
+      document.querySelectorAll('.jslider-slide')[i].addEventListener("touchend", (e) => {
         this.touchEnd = 0;
         this.touch = false;
       });
-      document.getElementsByClassName('jslider-slide')[i].addEventListener("touchmove", (e) => {
-        try{
-          if(this.touch == true){
+      document.querySelectorAll('.jslider-slide')[i].addEventListener("touchmove", (e) => {
+        try {
+          if (this.touch == true) {
             this.touchEnd = this.touchStart - e.touches[0].clientX;
-            console.log(Math.abs(this.touchEnd), this.slideWidth/2);
-            if(Math.abs(this.touchEnd) >= this.slideWidth/2){
+            if (Math.abs(this.touchEnd) >= this.slideWidth / 2) {
               this.touchEnd < 0 ? this.currentSlide-- : this.currentSlide++;
               this.touch = false;
             }
           }
         } catch (e) {}
-        if(this._settings.infinite == true){
-          if(this.currentSlide < 0) this.currentSlide=this.slidesCount-this._settings.showElements;
-          if(this.currentSlide > this.slidesCount-this._settings.showElements) this.currentSlide=0;
+        if (this._settings.infinite == true) {
+          if (this.currentSlide < 0) this.currentSlide = this.slidesCount - this._settings.showElements;
+          if (this.currentSlide > this.slidesCount - this._settings.showElements) this.currentSlide = 0;
         } else {
-          if(this.currentSlide < 0) this.currentSlide=0;
-          if(this.currentSlide > this.slidesCount-this._settings.showElements) this.currentSlide=this.slidesCount-this._settings.showElements;
+          if (this.currentSlide < 0) this.currentSlide = 0;
+          if (this.currentSlide > this.slidesCount - this._settings.showElements) this.currentSlide = this.slidesCount - this._settings.showElements;
         }
         this.scrollTo(this.currentSlide);
       });
